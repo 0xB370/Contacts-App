a874aa5cf26e3a2598cdf62f0f9098f42578d1e3
Beto-23 <mansilla_alberto@outlook.com>
2020-05-02 02:25:31 -0300

    Deploying 8

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,66 @@ const { Router } = require('express');
 const router = Router();
 require("firebase/auth");
 require("firebase/firestore");
-
-
 const admin = require('firebase-admin');
+
+const {auth} = require('google-auth-library');
+
+
+
+
+
+
+
+
+
+
+
+// load the environment variable with our keys
+const keysEnvVar = process.env['CREDS'];
+if (!keysEnvVar) {
+  throw new Error('The $CREDS environment variable was not found!');
+}
+const keys = JSON.parse(keysEnvVar);
+
+async function main() {
+  // load the JWT or UserRefreshClient from the keys
+  const client = auth.fromJSON(keys);
+  client.scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+  const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
+  const res = await client.request({url});
+  //console.log(res.data);
+}
+
+main().catch(console.error);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.cert(keys)
 });
 
 const db = admin.firestore();
+
+
+
+
+
+
+
+
+
 
 
 
